69c65996acefc3e4642c7bd0a6ed7ea441242f24
hangyubin <hangyubin@163.com>
2025-09-22 19:39:45 +0800

    Update diy-18.06.sh

--- diy-18.06.sh
+++ diy-18.06.sh
@@ -227,6 +227,8 @@ fi
 
 # 开始更新&安装插件
 begin_time=$(date '+%H:%M:%S')
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 ./scripts/feeds update -a 1>/dev/null 2>&1
 ./scripts/feeds install -a 1>/dev/null 2>&1
 status "更新&安装插件"
