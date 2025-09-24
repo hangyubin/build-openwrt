b5981dcb74aab533af9bd94709a10707691fb941
hangyubin <hangyubin@163.com>
2025-09-24 11:35:47 +0800

    Update diy-23.05.sh

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -224,6 +224,10 @@ fi
 
 # 开始更新&安装插件
 begin_time=$(date '+%H:%M:%S')
+echo >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 ./scripts/feeds update -a 1>/dev/null 2>&1
 ./scripts/feeds install -a 1>/dev/null 2>&1
 status "更新&安装插件"
